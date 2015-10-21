1131bdc6a61b9fe8f04bb939e75bafd6d03d3129
Joseph Larson <kf2617gi@go.minneapolis.edu>
2015-10-21 15:47:41 -0500

    Built base program

---
+++ InventoryManagement/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace InventoryManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbMake = new System.Windows.Forms.ComboBox();
+            this.cmbModel = new System.Windows.Forms.ComboBox();
+            this.cmbPart = new System.Windows.Forms.ComboBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.lstParts = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phone Make: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Part: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Color: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Quantity: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(211, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Model: ";
+            // 
+            // cmbMake
+            // 
+            this.cmbMake.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMake.FormattingEnabled = true;
+            this.cmbMake.Items.AddRange(new object[] {
+            "Apple",
+            "Nokia",
+            "Samsung"});
+            this.cmbMake.Location = new System.Drawing.Point(93, 13);
+            this.cmbMake.Name = "cmbMake";
+            this.cmbMake.Size = new System.Drawing.Size(112, 21);
+            this.cmbMake.TabIndex = 5;
+            this.cmbMake.SelectedIndexChanged += new System.EventHandler(this.ChangeModelCollection);
+            // 
+            // cmbModel
+            // 
+            this.cmbModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbModel.FormattingEnabled = true;
+            this.cmbModel.Location = new System.Drawing.Point(259, 13);
+            this.cmbModel.Name = "cmbModel";
+            this.cmbModel.Size = new System.Drawing.Size(96, 21);
+            this.cmbModel.TabIndex = 6;
+            // 
+            // cmbPart
+            // 
+            this.cmbPart.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPart.FormattingEnabled = true;
+            this.cmbPart.Items.AddRange(new object[] {
+            "Battery",
+            "Charging Port",
+            "Display",
+            "Home Button",
+            "Other",
+            "Power / Lock Button"});
+            this.cmbPart.Location = new System.Drawing.Point(93, 40);
+            this.cmbPart.Name = "cmbPart";
+            this.cmbPart.Size = new System.Drawing.Size(112, 21);
+            this.cmbPart.Sorted = true;
+            this.cmbPart.TabIndex = 7;
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(93, 69);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(96, 20);
+            this.txtColor.TabIndex = 8;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(93, 97);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(26, 20);
+            this.txtQuantity.TabIndex = 9;
+            // 
+            // lstParts
+            // 
+            this.lstParts.FormattingEnabled = true;
+            this.lstParts.Location = new System.Drawing.Point(15, 188);
+            this.lstParts.Name = "lstParts";
+            this.lstParts.Size = new System.Drawing.Size(423, 95);
+            this.lstParts.TabIndex = 10;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(15, 147);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(112, 146);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 12;
+            this.btnRemove.Text = "&Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(214, 147);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 13;
+            this.btnSort.Text = "&Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 295);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstParts);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.cmbPart);
+            this.Controls.Add(this.cmbModel);
+            this.Controls.Add(this.cmbMake);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Inventory Management";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbMake;
+        private System.Windows.Forms.ComboBox cmbModel;
+        private System.Windows.Forms.ComboBox cmbPart;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.ListBox lstParts;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnSort;
+    }
+}
+
---
+++ InventoryManagement/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/****************************************************************************
+ * This is an inventory management system that tracks phone parts for a
+ * retail/repair store.  It is under development.
+ *
+ * Produced by: Joseph Larson, Michael Williams, Arbin Rai
+ * **************************************************************************/
+
+namespace InventoryManagement
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Global list that holds all parts that are added
+        //changes are made to this list and then this list is
+        //displayed in the listbox control
+        List<string> partsList = new List<string> { };
+      
+
+        //Method to add parts takes in the info from the combo and txtboxes
+        //and generates a string description of the part, then adds it to the
+        //global list
+        private void AddPart()
+        {
+            string make = cmbMake.Text;
+            string model = cmbModel.Text;
+            string part = cmbPart.Text;
+            string color = txtColor.Text;
+            int quantity = Convert.ToInt16(txtQuantity.Text);
+            DateTime currentDateTime = DateTime.Now;
+
+            string fullPart = make + " " + model + " " + part + ", " + color + ", Recieved: " + currentDateTime.ToShortDateString();
+
+            for (int i = 0; i < quantity; i++)
+            {
+                partsList.Add(fullPart);
+            }
+        }
+
+        //adds each item in the global parts list to the listbox control
+        private void UpdateListBox()
+        {
+            foreach (string part in partsList)
+                lstParts.Items.Add(part);
+        }
+
+
+
+        //remove the selected part
+        private void RemovePart()
+        {
+            int index = lstParts.SelectedIndex;
+            partsList.RemoveAt(index);
+        }
+
+        //clears all the items in the listbox
+        private void ClearListBox()
+        {
+            lstParts.Items.Clear();
+        }
+
+
+        //sorts the global list
+        private void SortList()
+        {
+            partsList.Sort();
+        }
+
+
+
+
+
+        private void ClearForm()
+        {
+            txtColor.Clear();
+            txtQuantity.Clear();
+            cmbModel.Refresh();
+            cmbMake.Refresh();
+            cmbPart.ResetText();
+            cmbMake.Focus();
+
+        }
+
+        private void ChangeModelCollection(object sender, EventArgs e)
+        {
+            cmbModel.Items.Clear();
+            string make = cmbMake.Text;
+
+            switch (make)
+            {
+                case "Apple":
+                cmbModel.Items.Add("iPhone 5C");
+                cmbModel.Items.Add("iPhone 5S");
+                cmbModel.Items.Add("iPhone 6");
+                cmbModel.Items.Add("iPhone 6+");
+                break;
+                case "Samsung":
+                cmbModel.Items.Add("Galaxy S4");
+                cmbModel.Items.Add("Galaxy S5");
+                cmbModel.Items.Add("Galaxy Note III");
+                cmbModel.Items.Add("Galaxy Note IV");
+                break;
+                case "Nokia":
+                cmbModel.Items.Add("Lumia 925");
+                cmbModel.Items.Add("Lumia 1020");
+                cmbModel.Items.Add("Lumia 1520");
+
+                break;
+            }
+        }
+
+        //add part to the global part list, clear the list box, then
+        //update the listbox with the new global list
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddPart();
+            ClearListBox();
+            UpdateListBox();
+            ClearForm();
+        }
+        //removes part from global list, clears list box, updates listbox
+
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            SortList();
+            ClearListBox();
+            UpdateListBox();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            RemovePart();
+            ClearListBox();
+            UpdateListBox();
+        }
+
+
+
+
+    }
+}
